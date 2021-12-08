1864f15de6b81cbf580334ac367f67d390ddd863
m8u <mzgtootoo@gmail.com>
2021-12-08 14:49:50 +0700

    Improved compatibility with Minimal Twitter add-on

--- content.js
+++ content.js
@@ -2,16 +2,15 @@ function getAllElementsWithAttribute(attribute) {
     var matchingElements = [];
     var allElements = document.getElementsByTagName('*');
     for (var i = 0, n = allElements.length; i < n; i++) {
-        if (allElements[i].getAttribute(attribute) !== null)
-        {
+        if (allElements[i].getAttribute(attribute) !== null) {
             matchingElements.push(allElements[i]);
         }
     }
     return matchingElements;
 }
 
-var style = document.getElementsByTagName("style")[0];
-style.innerHTML += "@keyframes paddingBottomToHundred { from { padding-bottom: 56.25%; } to { padding-bottom: 100%; } }"
+// var style = document.getElementsByTagName("style")[0];
+// style.innerHTML += "@keyframes paddingBottomToHundred { from { padding-bottom: 56.25%; } to { padding-bottom: 100%; } }"
 
 let classNamesBlacklist = ["css-1dbjc4n r-13awgt0",
                         "css-9pa8cd",
@@ -23,25 +22,24 @@ let classNamesBlacklist = ["css-1dbjc4n r-13awgt0",
                         "r-8akbif r-orgf3d r-1udh08x r-u8s1d r-xjis5s r-1wyyakw",
                         "css-1dbjc4n r-1pz39u2 r-16y2uox r-1wbh5a2"]
 
-var observer = new MutationObserver(function(mutations) {
+var observer = new MutationObserver(async function(mutations) {
     if (window.location.href == "https://twitter.com/compose/tweet"
     || window.location.href.includes("https://twitter.com/messages"))
         return;
 
-    mutations.forEach(function(mutation) {
-        if (!mutation.addedNodes) return;
+    for (const mutation of mutations) {
+        if (!mutation.addedNodes) continue;
         try {
-            if (classNamesBlacklist.includes(mutation.addedNodes[0].className)) return;
-            //console.log(mutation.addedNodes[0].className);
-        } catch (e){
-            return;
+            if (classNamesBlacklist.includes(mutation.addedNodes[0].className)) continue;
+        } catch (e) {
+            continue;
         }
 
         var elementsWithSrc = getAllElementsWithAttribute("src");
-        for (var i = 0; i<elementsWithSrc.length; i++) {
-            if (elementsWithSrc[i].src.includes("/media/") 
-                && elementsWithSrc[i].getBoundingClientRect().top + (document.documentElement.scrollTop || document.body.scrollTop)
-                                                > (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientHeight/2) {
+        for (var i = 0; i < elementsWithSrc.length; i++) {
+            if (elementsWithSrc[i].src.includes("/media/")) {
+                //&& elementsWithSrc[i].getBoundingClientRect().top + (document.documentElement.scrollTop || document.body.scrollTop)
+                //                                > (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientHeight/2) {
 
                 if (elementsWithSrc[i].naturalWidth == 0) continue;
 
@@ -58,19 +56,19 @@ var observer = new MutationObserver(function(mutations) {
 
                 //console.log(elementsWithSrc[i].src+"   "+elementsWithSrc[i].naturalWidth+"x"+elementsWithSrc[i].naturalHeight);
                 var imgSizeRatio = elementsWithSrc[i].naturalWidth / elementsWithSrc[i].naturalHeight;
-                var newPadding = 507 / imgSizeRatio;
+                var newPadding = thatNode.getBoundingClientRect().width / imgSizeRatio;
 
                 if (thatNode.getAttribute("style") == "padding-bottom: 56.25%;") {
-                    thatNode.setAttribute("style", "padding-bottom: "+newPadding+"px; animation: paddingBottomToHundred; animation-duration: .3s;");
+                    thatNode.setAttribute("style", "padding-bottom: "+newPadding+"px");// animation: paddingBottomToHundred; animation-duration: .3s;");
                 }
             }
         }
-    })
+    }
 })
 
 observer.observe(document.body, {
-    childList: true
-    , subtree: true
-    , attributes: false
-    , characterData: false
+    childList: true, 
+    subtree: true, 
+    attributes: false, 
+    characterData: false
 })
